bda3c391187e807b78636ce3574b574526a85913
N1kSt4r <nikita2000tsuranov@gmail.com>
2019-03-10 02:42:28 +0300

    add beta-pop-up window with photo

--- src/script.js
+++ src/script.js
@@ -24,11 +24,12 @@ function showAlert() {
 }
 
 function compare(photoPost1, photoPost2) {
-  if (photoPost1.descriprion != null && photoPost1.descriprion != photoPost2.descriprion) 
+  if (photoPost1 == undefined) {
+    return true;
+  }
+  if (photoPost1.author !== undefined && photoPost1.author != photoPost2.author) 
     return false;
-  if (photoPost1.author != null && photoPost1.author != photoPost2.author) 
-    return false;
-  if (photoPost1.createdAt != null && photoPost1.createdAt != photoPost2.createdAt) 
+  if (photoPost1.createdAt !== undefined && photoPost1.createdAt != photoPost2.createdAt) 
     return false;
   return true;
 }
@@ -41,8 +42,11 @@ function getPhotoPosts(gallery, skip, top, filterConfig) {
     top = 10;
   }
   result = [];
-  for (i = skip; i < skip + top && i < gallery.length; ++i) {
-    addPhotoPost(result, gallery[i]);
+  for (i = skip; top != 0 && i < gallery.length; ++i) {
+    if (compare(filterConfig, gallery[i])) {
+      addPhotoPost(result, gallery[i]);
+      --top;
+    }
   }
   return result;
 }
@@ -75,7 +79,8 @@ function removePhotoPost(gallery, id) {
 
 
 var descriprions = ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime magnam libero similique.', 'Molestiae vel rem cumque reiciendis adipisci nesciunt perspiciatis aspernatur aliquam nemo incidunt laborum dolorum quae!', 'Maxime magnam libero similique, molestiae vel rem cumque reiciendis adipisci nesciunt perspiciatis.'];
-var photoLinks = ['./img/users.svg', './img/userM.svg', './img/userF.svg'];
+var photoLinks = ['./img/users.svg', './img/userM.svg', './img/userF.svg', './img/photo.svg'];
+var photoAuthors = ['Valentin Dytin', 'Vladimir Putin', 'Vladislav Nytin'];
 
 var photoPosts = [];
 
@@ -83,32 +88,31 @@ for (i = 0; i < 20; ++i) {
   photoPosts[i] = {};
   photoPosts[i].id = i + 1 + '';
   photoPosts[i].descriprion = descriprions[Math.round(Math.random() * 100) % 3];
-  photoPosts[i].photoLink = photoLinks[(Math.round(Math.random() * 100)) % 3];
+  photoPosts[i].photoLink = photoLinks[(Math.round(Math.random() * 100)) % 4];
   photoPosts[i].createdAt = new Date('2018-02-23T23:00:00');
-  photoPosts[i].author = 'User Name';
-  /*var temp = document.createElement('img');
-  temp.className = 'gallery__photo';
-  temp.src = photoPosts[i].photoLink;
-  document.getElementsByClassName('gallery')[0].appendChild(temp);*/
+  photoPosts[i].author = photoAuthors[Math.round(Math.random() * 100) % 3];
 };
 
-getPhotoPosts(photoPosts, 0, 6).forEach(function(element) {
+getPhotoPosts(photoPosts, 0, 6).forEach((element) => {
   var temp = document.createElement('img');
   temp.className = 'gallery__photo';
   temp.src = element.photoLink;
+  temp.id = element.id;
   document.getElementsByClassName('gallery')[0].appendChild(temp);
 });
 
 var shown = 6;
 if (shown >= photoPosts.length) {
-  document.getElementsByClassName('gallery__button')[0].style.display = 'none';
+  //first match
+  document.querySelector('.gallery__button').style.display = 'none';
 };
 
 function loadMore() {
-  getPhotoPosts(photoPosts, shown, 9).forEach(function(element) {
-    var temp = document.createElement('img');
+  getPhotoPosts(photoPosts, shown, 9/*,  {author: 'Valentin Dytin'}*/).forEach((element) => {
+    var temp = document.createElement('IMG');
     temp.className = 'gallery__photo';
     temp.src = element.photoLink;
+    temp.id = element.id;
     document.getElementsByClassName('gallery')[0].appendChild(temp);
   });
   shown = shown + 9;
@@ -117,7 +121,76 @@ function loadMore() {
   };
 };
 
+document.querySelector('.gallery').addEventListener('click', (event) => {
+  if (event.target.nodeName !== 'IMG') return;
+  const img = event.target;
 
+  const form = createImageForm(img);
+  document.body.appendChild(form);
+});
 
+function createElement (nodeName, classes, attrs = []) {
+  var element = document.createElement(nodeName);
+  classes.forEach(className => element.classList.add(className));
+  attrs.forEach(attr => element.setAttribute(attr[0], attr[1]));
+
+  return element;
+}
+
+function createImageForm(img) {
+  var photoPost = getPhotoPost(photoPosts, img.id);
+  var overlay = createElement('div', ['image-form-overlay', 'flex', 'transitable-opacity'], [['id', 'overlay']]);
+  
+  overlay.addEventListener('click', (evt) => {
+    if (evt.target.id !== 'overlay') return;
+    document.body.removeChild(overlay);
+  }, false);
+
+  var contentWrapper = createElement('div', ['image-form__content', 'grid', 'transitable-opacity']);
+  
+  var image = createElement('img', ['image-form__image'], [['src', img.src]])
+  contentWrapper.appendChild(image);
+  
+  var imageInfo = createElement('div', ['image-form__info']);
+
+  var imageAuthor = createElement('div', ['image-form__info__author']);
+  var imageAuthorName = createElement('p', ['image-form__info__author__content']);
+  imageAuthorName.innerHTML = photoPost.author;
+  var imageAuthorDate = createElement('p', ['image-form__info__author__content']);
+  imageAuthorDate.innerHTML = (photoPost.createdAt + '').split('G')[0];
+  imageAuthor.appendChild(imageAuthorName);
+  imageAuthor.appendChild(imageAuthorDate);
+  imageInfo.appendChild(imageAuthor);
+
+  var imageDescription = createElement('p', ['image-form__info__description']);
+  imageDescription.innerHTML = descriptionWidth(photoPost.descriprion);
+  imageInfo.appendChild(imageDescription);
+
+  contentWrapper.appendChild(imageInfo);
+  
+  overlay.appendChild(contentWrapper);
+
+  setTimeout(() => {
+    overlay.style.opacity = '1';
+    contentWrapper.style.opacity = '1';
+  }, 0);
+
+  return overlay;
+}
+
+function descriptionWidth(string) {
+  var length = 0;
+  var words = string.split(' ');  
+  var ans = '';
+  words.forEach(word => {
+    length = length + word.length;
+    if (length > 40) {
+      ans = ans + '<br>';
+      length = 0;
+    }
+    ans = ans + word + ' ';
+  });
+  return ans;
+}
 
 /*alert(validatePhotoPost(photoPosts[10]));*/
